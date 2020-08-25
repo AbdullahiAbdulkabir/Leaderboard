e56254113da2f0dff7ab65aeaec4c70e66f297b4
AbdullahiAbdulkabir <abdullahiabdulkabir1@gmail.com>
2020-08-25 08:38:01 +0100

    keys

--- config/conn.php
+++ config/conn.php
@@ -1,8 +1,8 @@
 <?php
-$hostname = 'us-cdbr-east-02.cleardb.com';
-$username = 'b54ae81fb3204a';
-$password = '13c1b720';
-$db_name = 'heroku_aeb9761bff98a50';
+$hostname = 'd';
+$username = 'a';
+$password = 'b';
+$db_name = 'a';
 
 $conn = mysqli_connect($hostname,$username,$password,$db_name);
 if (!$conn) {
--- config/connect.php
+++ config/connect.php
@@ -1,8 +1,8 @@
 <?php
-$hostname = 'localhost';
-$username = 'root';
-$password = '';
-$db_name = 'leaderboard';
+$hostname = 'us-cdbr-east-02.cleardb.com';
+$username = 'b54ae81fb3204a';
+$password = '13c1b720';
+$db_name = 'heroku_aeb9761bff98a50';
 
 $conn = mysqli_connect($hostname,$username,$password,$db_name);
 if (!$conn) {
